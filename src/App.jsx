f209db1e315ce5fbae8f5cad84911e556c9d19f8
sarebok <bortonae@hotmail.com>
2023-11-16 13:17:07 -0300

    inicial setup of components and views

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { Router, Routes, Route } from "react-router-dom";
+import Home from "./assets/views/Home";
+import Pokemones from "./assets/views/Pokemones";
+import NotFound from "./assets/views/NotFound";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/pokemones" element={<Pokemones />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/assets/views/Pokemones.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Pokemones = () => {
+  return <div>Selecciona un pokemon</div>;
+};
+
+export default Pokemones;
